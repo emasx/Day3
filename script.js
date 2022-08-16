0272bec08eca836fe92308f60b1d4f5908fc39af
emasx <29961897+emasx@users.noreply.github.com>
2022-08-16 23:28:53 +0300

    Day 3

---
+++ script.js
@@ -0,0 +1,21 @@
+/* let   = we use the let keyword to declare variables that can change later so basically during the execution of our program
+   const = we use the const keyword to declare variables that are not supposed to change at any point in the future
+   
+*/
+
+
+//     1.Set the value of 'language' to the language spoken where you live (some
+//     countries have multiple languages, but just choose one)
+//     2. Think about which variables should be const variables (which values will never
+//     change, and which might change?). Then, change these variables to const.
+//     3. Try to change one of the changed variables now, and observe what happens
+
+const language = 'romanian';
+const country  = 'Romania';
+const continent = 'Europe';
+const isIsland = false;
+
+console.log(language);
+console.log(country);
+console.log(continent);
+console.log(isIsland);
